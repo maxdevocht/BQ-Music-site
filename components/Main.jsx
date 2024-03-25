847edc44b286b277a69f40485968c666a4b75c84
Max de Vocht <maxdevocht@hotmail.com>
2024-03-25 16:19:05 +0100

    Update
    Added Main component

---
+++ components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <div className="content">
+                <div className="work">
+                    <h2 id="work">Recent Werk</h2>
+                </div>
+            </div>
+        </main>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import Sidebar from '../components/Sidebar.jsx'
-import Navbar from '../components/Navbar.jsx'
 import Header from '../components/Header.jsx'
+import Main from '../components/Main.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <>
       <Header />
-      <Sidebar />
+      <Main />
     </>
   )
 }
