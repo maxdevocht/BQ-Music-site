b00b0a90519eda3088645007c33283bb1f686bb6
Max de Vocht <maxdevocht@hotmail.com>
2024-03-25 15:03:11 +0100

    Update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Sidebar from '../components/Sidebar.jsx'
+import Navbar from '../components/Navbar.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <div>
       <Sidebar />
+      <Navbar />
     </div>
   )
 }
