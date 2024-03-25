6e73a45c081802d69fbffc9c1207be3594f2d1fb
Max de Vocht <maxdevocht@hotmail.com>
2024-03-25 15:59:47 +0100

    Update
    Added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Sidebar from '../components/Sidebar.jsx'
 import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <>
+      <Header />
       <Sidebar />
-      <Navbar />
     </>
   )
 }
