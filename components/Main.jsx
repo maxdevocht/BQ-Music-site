e52e20fa4284ff1822ace0d5d6232d4fb78b1bfe
Max de Vocht <maxdevocht@hotmail.com>
2024-03-26 10:21:48 +0100

    Update css
    Changing classnames in work div

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,8 +4,11 @@ export default function Main() {
     return (
         <main>
             <div className="content">
-                <div className="work">
-                    <h2 id="work" className="section--title">Recent Werk</h2>
+                <div className="section--work">
+                    <div className="section--label">
+                        <h2 id="work" className="label">Recent Werk</h2>
+                    </div>
+                    <h1 className="section--title">SansAmp Overdrive & Aguilar TLC Compressor</h1>
                 </div>
             </div>
         </main>
