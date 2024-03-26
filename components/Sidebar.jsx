c9b20c75ed6680da5c6ca397bb504bb4650ec828
Max de Vocht <maxdevocht@hotmail.com>
2024-03-26 19:26:32 +0100

    Update
    Added sidebar styling

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,15 +5,6 @@ export default function Sidebar() {
         <section id='sidebarSection' className='sidebar'>
             <div className='home'>
                 <ul>
-                    {/* <li>
-                        <a href='#home'>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#f2f2f2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M18 6l-12 12"></path>
-                            <path d="M6 6l12 12"></path>
-                            </svg>
-                        </a>
-                    </li> */}
                     <li className='sidebar--items'><a href="#home">_Home</a></li>
                     <li className='sidebar--items'><a href="#work">_Recent Werk</a></li>
                     <li className='sidebar--items'><a href="#review">_Reviews</a></li>
