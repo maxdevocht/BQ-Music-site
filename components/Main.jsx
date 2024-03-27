07130598a6125633bc2a1e023f59f0477a885c66
Max de Vocht <maxdevocht@hotmail.com>
2024-03-27 11:22:12 +0100

    Update main.jsx
    Added divs around section--title

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,13 +8,27 @@ export default function Main() {
                     <div className="section--label">
                         <h2 id="work" className="label">Recent Werk</h2>
                     </div>
-                    <h1 className="section--title">SansAmp Overdrive & Aguilar TLC Compressor</h1>
-                    <h1 className="section--title">Jazzbass X-blend Wiring</h1>
-                    <h1 className="section--title">Custom-made PreAmps</h1>
-                    <h1 className="section--title">Brooks Bass; Master Volume and Dual Output</h1>
-                    <h1 className="section--title">POT Overdrive; based on the King of Tone</h1>
-                    <h1 className="section--title">SWUNG Guitar Wiring</h1>
-                    <h1 className="section--title">Wiring Harness</h1>
+                    <div className="card">
+                        <h1 className="section--title">SansAmp Overdrive & Aguilar TLC Compressor</h1>
+                    </div>
+                    <div className="card">
+                        <h1 className="section--title">Jazzbass X-blend Wiring</h1>
+                    </div>
+                    <div className="card">
+                        <h1 className="section--title">Custom-made PreAmps</h1>
+                    </div>
+                    <div className="card">
+                        <h1 className="section--title">Brooks Bass; Master Volume and Dual Output</h1>
+                    </div>
+                    <div className="card">
+                        <h1 className="section--title">POT Overdrive; based on the King of Tone</h1>
+                    </div>
+                    <div className="card">
+                        <h1 className="section--title">SWUNG Guitar Wiring</h1>
+                    </div>
+                    <div className="card">
+                        <h1 className="section--title">Wiring Harness</h1>
+                    </div>
                 </div>
                 <div className="section--categorie">
                     <div className="section--label">
